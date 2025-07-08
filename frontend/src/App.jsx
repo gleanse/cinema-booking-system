0a76a86405b897e767dc599fd0dd3fad17147b10
gleanse <auldeyjerusalem2@gmail.com>
2025-07-09 06:56:05 +0800

    (FRONTEND) install  axios and tailwindcss
    after the installation i  test if tailwindcss is working fine by creating simple component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-indigo-500 via-blue-500 to-blue-950 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        tailwind test if working
+      </h1>
+    </div>
+  );
+}
+
+export default App;
