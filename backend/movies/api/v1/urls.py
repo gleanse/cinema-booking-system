67b6c8253fdf65c1b09592396c45991392db3c7b
gleanse <auldeyjerusalem2@gmail.com>
2025-07-18 19:39:13 +0800

    (BACKEND) refresher only basic methods for genre

--- backend/movies/api/v1/urls.py
+++ backend/movies/api/v1/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MovieList
+from .views import MovieList, GenreList
 
 urlpatterns = [
-    path('movies/',MovieList.as_view(), name='movie-list')
+    path('movies/',MovieList.as_view(), name='movie-list'),
+    path('genres/',GenreList.as_view(), name='genre-list'),
 ]
--- backend/movies/api/v1/views.py
+++ backend/movies/api/v1/views.py
@@ -9,12 +9,25 @@ from .serializers import (
     GenreSerializer,
 )
 
+class GenreList(APIView):
+        def post(self, request):
+                serializer = GenreSerializer(data=request.data)
+                if serializer.is_valid():
+                        serializer.save()
+                        return Response(serializer.data, status=status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        def get(self, request):
+                genres = Genre.objects.all()
+                serializer = GenreSerializer(genres, many=True)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+
 
 class MovieList(APIView):
         def get(self, request):
                 movies = Movie.objects.all()
                 serializer = MovieSerializer(movies, many=True)
-                return Response(serializer.data)
+                return Response(serializer.data, status=status.HTTP_200_OK)
         
         def post(self, request):
                 serializer = MovieSerializer(data=request.data)
