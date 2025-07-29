5f7eef2d3f2c0b1c92a459b246b1d734349ee20f
gleanse <auldeyjerusalem2@gmail.com>
2025-07-29 16:16:05 +0800

    (BACKEND) basic CRUD views methods for movies
    complete basic CRUD views methods of the model movies with some using query parameter to choose what serializer to use for lightweight response or full fields response also added the endpoints in urls, also fixed the spacing indention i didnt notice it was set to 8 spaces cause other files set to default 4

--- backend/movies/api/v1/urls.py
+++ backend/movies/api/v1/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MovieList, GenreList
+from .views import MovieListView, GenreListView, MovieDetailView
 
 urlpatterns = [
-    path('movies/',MovieList.as_view(), name='movie-list'),
-    path('genres/',GenreList.as_view(), name='genre-list'),
+    path('movies/',MovieListView.as_view(), name='movie-list'),
+    path('movies/<int:pk>/',MovieDetailView.as_view(), name='movie-detail'),
+    path('genres/',GenreListView.as_view(), name='genre-list'),
 ]
--- backend/movies/api/v1/views.py
+++ backend/movies/api/v1/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import NotFound  
+
 from movies.models import Movie, Genre
 from .serializers import (
     MovieListSerializer,
@@ -9,29 +11,75 @@ from .serializers import (
     GenreSerializer,
 )
 
-class GenreList(APIView):
-        def post(self, request):
-                serializer = GenreSerializer(data=request.data)
-                if serializer.is_valid():
-                        serializer.save()
-                        return Response(serializer.data, status=status.HTTP_201_CREATED)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-        def get(self, request):
-                genres = Genre.objects.all()
-                serializer = GenreSerializer(genres, many=True)
-                return Response(serializer.data, status=status.HTTP_200_OK)
+class GenreListView(APIView):
+    def post(self, request):
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class MovieList(APIView):
-        def get(self, request):
-                movies = Movie.objects.all()
-                serializer = MovieSerializer(movies, many=True)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-        
-        def post(self, request):
-                serializer = MovieSerializer(data=request.data)
-                if serializer.is_valid():
-                        serializer.save()
-                        return Response(serializer.data, status=status.HTTP_201_CREATED)
-                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class MovieListView(APIView):
+    # show all fields of each movie in list of movies
+    def get(self, request):
+        movies = Movie.objects.all()
+        mode = request.query_params.get("fields")
+
+        if mode == "full":
+            serializer = MovieSerializer(movies, many=True)
+        else:
+            serializer = MovieListSerializer(movies, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MovieDetailView(APIView):
+    def get_object(self, pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            raise NotFound(detail="Movie not found")
+        return movie
+    
+    def get(self, request, pk):
+        movie = self.get_object(pk)
+        mode = request.query_params.get("fields")
+
+        if mode == "full":
+            serializer = MovieSerializer(movie)
+        else:
+            serializer = MovieListSerializer(movie)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def patch(self, request, pk):
+        movie = self.get_object(pk)
+        serializer = MovieSerializer(movie, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def put(self, request, pk):
+        movie = self.get_object(pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        movie = self.get_object(pk)
+        movie.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
